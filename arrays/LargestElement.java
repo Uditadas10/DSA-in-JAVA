6fdacfd9fe25fc6f2519b972ff7718e7b9862a47
udita das <uditadas619@gmail.com>
2026-04-18 23:13:41 +0530

    Added largest element in array problem

---
+++ arrays/LargestElement.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class LargestElement {
+    public static void main(String args[]){
+        int arr[]={8,5,3,9,2,0};
+        int max=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+    System.out.println(max);
+    }
+
+}
